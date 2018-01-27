1048015f55cb3987f276c1efafd6a1923734c980
UndeadDeveloper <keelinty@gmail.com>
2018-01-27 17:45:08 -0600

    fix build issues

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -42,6 +42,13 @@ static bool sched_boost_on_input;
 module_param(sched_boost_on_input, bool, 0644);
 
 static bool sched_boost_active;
+<<<<<<< HEAD
+=======
+#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+static int dynamic_stune_boost = 0;
+module_param(dynamic_stune_boost, uint, 0644);
+#endif /* CONFIG_DYNAMIC_STUNE_BOOST */
+>>>>>>> 9dd4435... fix build issues
 
 static struct delayed_work input_boost_rem;
 static u64 last_input_time;
